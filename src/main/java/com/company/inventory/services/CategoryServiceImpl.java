3e112d38cf7c1d18f8c2fa2a6c25b1ea9b339278
John Arango <jfamdevelop@gmail.com>
2024-12-04 09:17:05 -0500

    servicio guardar categoria

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -62,4 +62,30 @@ public class CategoryServiceImpl implements ICategoryService {
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
+	@Override
+	@Transactional()
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+				Category categorySaved = categoryDao.save(category);
+				if (categorySaved != null) {
+					list.add(categorySaved);
+					response.setMetadata("Respuesta ok", "00", "Categoria guardada");
+					response.getCategoryResponse().setCategory(list);
+				} else {
+					response.setMetadata("Respuesta err", "-1", "Categoria no guardada");
+					return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+				}
+				
+		} catch (Exception e) {			
+			response.setMetadata("Respuesta err", "-1", "Error al grabar la categoria");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
 }
