615ddb313b03f3f681cb34f61aa8e03cbef6a778
John Arango <jfamdevelop@gmail.com>
2025-03-09 21:22:25 -0500

    Guardar productos

---
+++ src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -0,0 +1,63 @@
+package com.company.inventory.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.company.inventory.dao.ICategoryDao;
+import com.company.inventory.dao.IProductDao;
+import com.company.inventory.model.Category;
+import com.company.inventory.model.Product;
+import com.company.inventory.response.ProductResponseRest;
+
+@Service
+public class ProductServiceImpl implements IProductService {
+	
+	private ICategoryDao categoryDao;
+	private IProductDao productDao;
+
+	public ProductServiceImpl(ICategoryDao categoryDao, IProductDao productDao) {
+		super();
+		this.categoryDao = categoryDao;
+		this.productDao = productDao;
+	}
+
+	@Override
+	public ResponseEntity<ProductResponseRest> save(Product product, Long categoryId) {
+		ProductResponseRest response = new ProductResponseRest();
+		List<Product> list = new ArrayList<>();
+		
+		try {
+			Optional<Category> category = categoryDao.findById(categoryId);
+			if( category.isPresent() ) {
+				product.setCategory(category.get());
+			} else {
+				response.setMetadata("Respuesta err", "-1", "Categoria no encontrada asociada al producto");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			Product productSaved = productDao.save(product);
+			
+			if( productSaved != null ) {
+				list.add(productSaved);
+				response.getProduct().setProducts(list);
+				response.setMetadata("Respuesta ok", "00", "Producto guardado");
+			} else {
+				response.setMetadata("Respuesta err", "-1", "Producto no guardado");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.BAD_REQUEST);
+			}
+				
+		} catch ( Exception e ) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta err", "-1", "Error al guardar producto");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+	}
+
+}
