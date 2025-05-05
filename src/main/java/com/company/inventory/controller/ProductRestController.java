228213361bc017ff7741ad4c20b8a98ee0d108ee
John Arango <jfamdevelop@gmail.com>
2025-05-04 21:00:24 -0500

    Actualizar producto

--- src/main/java/com/company/inventory/controller/ProductRestController.java
+++ src/main/java/com/company/inventory/controller/ProductRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,6 +70,25 @@ public class ProductRestController {
 	@DeleteMapping("/products/{id}")
 	public ResponseEntity<ProductResponseRest> deleteById(@PathVariable Long id) {
 		ResponseEntity<ProductResponseRest> response = productService.deleteById(id);
+		return response;
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<ProductResponseRest> update(
+			@RequestParam("picture") MultipartFile picture,
+			@RequestParam("name") String name,
+			@RequestParam("price") Double price,
+			@RequestParam("quantity") int quantity,
+			@RequestParam("categoryId") Long  categoryID,
+			@PathVariable Long id) throws IOException {
+		
+		Product product = new Product();
+		product.setName(name);
+		product.setPrice(price);
+		product.setQuantity(quantity);
+		product.setPicture(Util.compressZLib(picture.getBytes()));
+		
+		ResponseEntity<ProductResponseRest> response = productService.update(product, categoryID, id);
 		return response;
 	}
 }
--- src/main/java/com/company/inventory/services/ProductServiceImpl.java
+++ src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -181,4 +181,54 @@ public class ProductServiceImpl implements IProductService {
 		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);	
 	}
 
+	@Override
+	@Transactional
+	public ResponseEntity<ProductResponseRest> update(Product product, Long categoryId, Long id) {
+		ProductResponseRest response = new ProductResponseRest();
+		List<Product> list = new ArrayList<>();
+		
+		try {
+			Optional<Category> category = categoryDao.findById(categoryId);
+			if( category.isPresent() ) {
+				product.setCategory(category.get());
+			} else {
+				response.setMetadata("Respuesta err", "-1", "Categoria no encontrada asociada al producto");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			Optional<Product> productSearch = productDao.findById(id);
+			
+			if( productSearch.isPresent() ) {
+				productSearch.get().setQuantity(product.getQuantity());
+				productSearch.get().setCategory(product.getCategory());
+				productSearch.get().setName(product.getName());
+				productSearch.get().setPicture(product.getPicture());
+				productSearch.get().setPrice(product.getPrice());
+				
+				Product productToUpdate = productDao.save(productSearch.get());
+				
+				if (productToUpdate != null ) {
+					list.add(productToUpdate);
+					response.getProduct().setProducts(list);
+					response.setMetadata("Respuesta ok", "00", "Producto actualizado");	
+				} else {
+					response.setMetadata("Respuesta ok", "00", "Producto no actualizado");
+					return new ResponseEntity<ProductResponseRest>(response, HttpStatus.BAD_REQUEST);
+				}
+				
+				
+			} else {
+				response.setMetadata("Respuesta err", "-1", "Producto no actualizado");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+				
+		} catch ( Exception e ) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta err", "-1", "Error al guardar producto");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+	}
+
 }
