b1ff9c9e4982296953d82e79170bb521407ce2f4
John Arango <jfamdevelop@gmail.com>
2025-05-04 20:24:24 -0500

    Listar todos los productos

--- src/main/java/com/company/inventory/controller/ProductRestController.java
+++ src/main/java/com/company/inventory/controller/ProductRestController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,18 @@ public class ProductRestController {
 	@GetMapping("/products/filter/{name}")
 	public ResponseEntity<ProductResponseRest> searchByName(@PathVariable String name) {
 		ResponseEntity<ProductResponseRest> response = productService.searchByName(name);
+		return response;
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<ProductResponseRest> search() {
+		ResponseEntity<ProductResponseRest> response = productService.search();
+		return response;
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<ProductResponseRest> deleteById(@PathVariable Long id) {
+		ResponseEntity<ProductResponseRest> response = productService.deleteById(id);
 		return response;
 	}
 }
--- src/main/java/com/company/inventory/services/ProductServiceImpl.java
+++ src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -122,11 +122,63 @@ public class ProductServiceImpl implements IProductService {
 			
 		} catch ( Exception e ) {
 			e.getStackTrace();
-			response.setMetadata("Respuesta err", "-1", "Error al buscar el producto por nombre");
+			response.setMetadata("Respuesta err", "-1", "Error al buscar el productos");
 			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);		
+	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<ProductResponseRest> deleteById(Long id) {
+		ProductResponseRest response = new ProductResponseRest();
+		
+		try {			
+			productDao.deleteById(id);
+			response.setMetadata("Respuesta ok", "00", "Producto eliminado");			
+		} catch ( Exception e ) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta err", "-1", "Error al eliminar el producto");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<ProductResponseRest> search() {
+		ProductResponseRest response = new ProductResponseRest();
+		List<Product> list = new ArrayList<>();
+		List<Product> listAux = new ArrayList<>();
+		
+		try {			
+			listAux = (List<Product>) productDao.findAll();
+					
+			if( listAux.size() > 0) {
+				
+				listAux.stream().forEach((p)-> {
+					byte[] imagenDescompressed = Util.decompressZLib(p.getPicture());
+					p.setPicture(imagenDescompressed);
+					list.add(p);
+				});				
+				
+				response.getProduct().setProducts(list);
+				response.setMetadata("Respuesta ok", "00", "Productos encontrados");
+			} else {
+				response.setMetadata("Respuesta err", "-1", "Productos no encontrados");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			
+		} catch ( Exception e ) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta err", "-1", "Error al buscar el producto por nombre");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);	
 	}
 
 }
